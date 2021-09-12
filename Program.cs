f22cd08f8406b3b542535034e7eecd0f0bbe4f32
vlmr-dev <vlmr.info@gmail.com>
2021-09-12 00:53:57 -0300

    Adiciona no arquivo Program.cs a primeira implementação de funcionalidade - Obter opçao de usuário

--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace Media
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();                           // Adicionado - inicio
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+               switch (opcaoUsuario)
+               {
+                   case "1":
+                        // TODO: adiciona aluno
+                        break;
+                   case "2":
+                        // TODO: Lista aluno
+                        break;
+                   case "3":
+                        // TODO: Calcula média geral
+                        break;
+                   default:
+                       throw new ArgumentOutOfRangeException();
+               }
+
+               opcaoUsuario = ObterOpcaoUsuario();  
+            }
+
         }
-    }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novos alunos");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            String opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+
+    }                                                                              // Adicionado - fim
 }
